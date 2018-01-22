45b125320601cec01e24d87a5bf99aaaa85b033d
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-22 17:14:28 +0200

    Tank almos done

---
+++ Teht1-3/Teht1-3/Tank.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Tank
+    {
+        private string name, type;
+        private int crewcount;
+        private float speed, speedmax;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int CrewCount
+        {
+            get { return crewcount; }
+            set
+            {
+                if (value <= 6 && value >= 2)
+                {
+                    crewcount = value;
+                }
+                else crewcount = 0;
+            }
+        }
+        public float Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    speed = value;
+                }
+
+
+            }
+        }
+        public float SpeedMax
+        {
+            get
+            {
+                return speedmax;
+            }
+            set { speedmax = 100; }
+        }
+
+        public float AccelerateTo()
+        {
+            Console.Write("Accelerate to: ");
+            Speed = float.Parse(Console.ReadLine());
+
+            return Speed;
+        }
+        public float SlowTo()
+        {
+            
+            Console.Write("Slow to: ");
+            Speed = float.Parse(Console.ReadLine());
+
+            return Speed;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Type: " + Type);
+            Console.WriteLine("Crewcount: " + CrewCount);
+            Console.WriteLine("Speed: " + Speed);
+        }
+    }
+}
---
+++ Teht1-3/Teht1-3/TestTank.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            Console.WriteLine("TestTank");
+            Tank test = new Tank();
+
+            test.Name = "TestTank 9000";
+            test.Type = "Panther";
+
+            Console.Write("Insert CrewCount: ");
+            test.CrewCount = int.Parse(Console.ReadLine());
+            test.PrintData();
+
+            test.AccelerateTo();
+            test.SlowTo();
+            
+            test.PrintData();
+            
+        }
+    }
+}
