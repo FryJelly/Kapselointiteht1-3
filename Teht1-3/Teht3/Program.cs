9dbd6c6470e2ab34f57de1aedc2fb4b2cc158e46
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-24 19:50:19 +0200

    it's done boii

--- Teht1-3/Teht3/Program.cs
+++ Teht1-3/Teht3/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*Tehtävänäsi on ohjelmoida yksinkertaisen vahvistimen toiminta, jolla voidaan kontrolloida äänenvoimakkuutta välillä 0-100.
+ *Toteuta Vahvistin-luokka ja tee pääohjelma, jolla luot olion Vahvistin-luokasta. Säädä ja kokeile vahvistinta eri äänenvoimakkuuksilla. 
+ *Käytä Vahvistin-luokassa get- ja set-aksessoreita. get-aksessori palauttaa äänenvoimakkuuden ja set-aksessori rajaa äänenvoimakkuuden arvoa. */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,11 @@ namespace Teht3
     {
         static void Main(string[] args)
         {
+            Amplifier AMP = new Amplifier();
+            while (true)
+            {
+                AMP.Changevolume();
+            }
         }
     }
 }
--- Teht1-3/Teht3/Vahvistin.cs
+++ Teht1-3/Teht3/Vahvistin.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Teht3
 {
-    class Vahvistin
+    class Amplifier
     {
+        public int volume;
+        public int Volume
+        {
+            get { return volume; }
+            set {
+                if (value <= 100 && value >= 0)
+                {
+                    volume = value;
+                    Console.WriteLine("The volume is now at: " + volume);
+
+                }
+                else
+                {
+                    if (value > 100)
+                    {
+                        Console.WriteLine("The volume is too damn high!");
+                        Console.WriteLine("The volume is now at: " + volume);
+                    }
+                    else
+                    {
+                        if (value < 0)
+                        {
+                            Console.WriteLine("The volume is too damn low!");
+                            Console.WriteLine("The volume is now at: " + volume);
+                        }
+                        else
+                        {
+                        }
+                    }
+                }
+            }
+        }
+
+        public int Changevolume()
+        {
+            int number;
+            Console.Write("Enter volume (0-100): ");
+            int.TryParse(Console.ReadLine(), out number);
+            Volume = number;
+            return Volume;
+        }
     }
 }
